69fdbe6960d32270d73f31ef8775d6858d9ebff0
ahmad-cod <ahmadadewale800@gmail.com>
2023-09-22 00:53:13 +0100

    update site

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -69,14 +69,14 @@ const Login = (props: Props) => {
         <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
           <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 '>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl '>
+              <h1 className='text-xl font-bold leading-tight tracking-tight text-primary/75 md:text-2xl '>
                 Sign in to your account
               </h1>
               <form className='space-y-4 md:space-y-6' onSubmit={handleLogin}>
                 <div>
                   <label
                     htmlFor='email'
-                    className='block mb-2 text-sm font-medium text-gray-900 '
+                    className='block mb-2 text-sm font-medium text-primary/80 '
                   >
                     Your email
                   </label>
@@ -84,7 +84,7 @@ const Login = (props: Props) => {
                     type='email'
                     name='email'
                     id='email'
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+                    className={`bg-gray-50 border border-gray-300 text-primary/80 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
                       emailError ? "border-red-500" : ""
                     }`}
                     placeholder='user@example.com'
@@ -104,7 +104,7 @@ const Login = (props: Props) => {
                 <div>
                   <label
                     htmlFor='password'
-                    className='block mb-2 text-sm font-medium text-gray-900 '
+                    className='block mb-2 text-sm font-medium text-primary/80 '
                   >
                     Password
                   </label>
@@ -113,7 +113,7 @@ const Login = (props: Props) => {
                     name='password'
                     id='password'
                     placeholder='••••••••'
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+                    className={`bg-gray-50 border border-gray-300 text-primary/80 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
                       passwordError ? "border-red-500" : ""
                     }`}
                     required
@@ -136,7 +136,7 @@ const Login = (props: Props) => {
                 )}
                 <button
                   type='submit'
-                  className={`w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${
+                  className={`w-full text-white hover:bg-primary/40 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${
                     isLoading
                       ? "bg-gray-400 cursor-not-allowed"
                       : "bg-primary dark:hover:bg-primary-700 dark:focus:ring-primary-800"
